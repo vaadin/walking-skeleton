5279d6f0b794e515a7de08003952360da4df1538
marcin <marcin@vaadin.com>
2025-03-31 15:01:51 +0200

    Removed Walking Skeleton UI generator project

--- walking-skeleton-generator/src/main/java/org/vaadin/walkingskeleton/generator/ProjectConfiguration.java
+++
@@ -1,7 +0,0 @@
-package org.vaadin.walkingskeleton.generator;
-
-record ProjectConfiguration(GroupId groupId,
-                            ArtifactId artifactId,
-                            PackageName basePackage,
-                            UIFramework uiFramework) {
-}
