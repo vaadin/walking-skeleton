c48237fc3ca1329107e5f208261787de75c35adc
marcin <marcin@vaadin.com>
2025-04-03 08:47:37 +0200

    Make Walking Skeleton default hybrid compilable project (#9)

--- walking-skeleton/src/main/java/com/example/application/todo/ui/view/TodoView.java
+++ walking-skeleton/src/main/java/com/example/application/todo/ui/view/TodoView.java
@@ -23,13 +23,9 @@ import java.util.Optional;
 
 import static com.vaadin.flow.spring.data.VaadinSpringDataHelpers.toSpringPageRequest;
 
-//#ifdef ui.hybrid
 @Route("task-list-flow")
-//#else
-@Route("")
-//#endif
-@PageTitle("Task List")
-@Menu(order = 0, icon = "vaadin:clipboard-check", title = "Task List")
+@PageTitle("Task List Flow")
+@Menu(order = 0, icon = "vaadin:clipboard-check", title = "Task List Flow")
 public class TodoView extends Main {
 
     private final TodoService todoService;
@@ -71,7 +67,7 @@ public class TodoView extends Main {
         addClassNames(LumoUtility.BoxSizing.BORDER, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
                 LumoUtility.Padding.MEDIUM, LumoUtility.Gap.SMALL);
 
-        add(new ViewToolbar("Task List", ViewToolbar.group(description, dueDate, createBtn)));
+        add(new ViewToolbar("Task List Flow", ViewToolbar.group(description, dueDate, createBtn)));
         add(todoGrid);
     }
 
