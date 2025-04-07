a77d2ac66e7202c2bbe3c84fe6658b67f357f442
Petter Holmstrom <petter@vaadin.com>
2025-04-07 13:59:43 +0300

    Change visibility of MainLayout and MainView constructors to prevent accidental reuse.

--- walking-skeleton/src/main/java/com/example/application/base/ui/view/MainView.java
+++ walking-skeleton/src/main/java/com/example/application/base/ui/view/MainView.java
@@ -1,5 +1,6 @@
 package com.example.application.base.ui.view;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.router.Route;
@@ -9,12 +10,19 @@ import com.vaadin.flow.theme.lumo.LumoUtility;
  * This view shows up when a user navigates to the root ('/') of the application.
  */
 @Route
-public class MainView extends Main {
+public final class MainView extends Main {
 
     // TODO Replace with your own main view.
 
-    public MainView() {
+    MainView() {
         addClassName(LumoUtility.Padding.MEDIUM);
         add(new Div("Please select a view from the menu on the left."));
+    }
+
+    /**
+     * Navigates to the main view.
+     */
+    public static void showMainView() {
+        UI.getCurrent().navigate(MainView.class);
     }
 }
