490be7702e3f248ac21b1acc727239552b41b9d3
Petter Holmstrom <petter@vaadin.com>
2025-05-12 10:53:43 +0300

    Update JavaDoc

--- walking-skeleton/src/test/java/com/example/application/TestApplication.java
+++ walking-skeleton/src/test/java/com/example/application/TestApplication.java
@@ -3,9 +3,8 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 
 /**
- * Run this application class to use Testcontainers for all your external services. This is convenient during
- * development as you don't have to have your database or message broker running locally. They will instead start
- * up in temporary Docker containers when you start the application.
+ * Run this application class to start your application locally, using Testcontainers for all external services.
+ * You have to configure the containers in {@link TestcontainersConfiguration}.
  */
 public class TestApplication {
 
