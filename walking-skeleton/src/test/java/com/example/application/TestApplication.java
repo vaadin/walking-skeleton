9cd91dcac0d853743f807c00f0d513f01e6c1659
Petter Holmstrom <petter@vaadin.com>
2025-05-27 12:08:20 +0300

    Reformatted by Spotless

--- walking-skeleton/src/test/java/com/example/application/TestApplication.java
+++ walking-skeleton/src/test/java/com/example/application/TestApplication.java
@@ -3,8 +3,9 @@ package com.example.application;
 import org.springframework.boot.SpringApplication;
 
 /**
- * Run this application class to start your application locally, using Testcontainers for all external services.
- * You have to configure the containers in {@link TestcontainersConfiguration}.
+ * Run this application class to start your application locally, using
+ * Testcontainers for all external services. You have to configure the
+ * containers in {@link TestcontainersConfiguration}.
  */
 public class TestApplication {
 
