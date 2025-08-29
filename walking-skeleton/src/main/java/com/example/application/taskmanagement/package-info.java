9fa830f24aa2e81d73523bfa9e8f56a9d6374fc2
Petter Holmström <petter@vaadin.com>
2025-08-29 14:55:49 +0300

    Simplify skeleton v24.9 (#152)
    * Simplify skeleton (#150)
    
    * Remove CC and security configuration
    
    * Remove Maven Wrapper plugin from generated project while keeping the scripts and config file.
    
    * Ignore index.html
    
    * Remove PostgreSQL and test containers
    
    * Remove package-info.java from base packages.
    
    * Remove package-info.java from taskmanagement.ui.view
    
    * Remove archunit tests
    
    * Remove Spring Boot Dev Tools
    
    * Remove code formatter from skeleton and clean up empty project POM
    
    * Reformat
    
    * Flatten taskmanagement package
    
    * Remove AbstractEntity because it is no longer needed
    
    * Flatten base.ui
    
    * Turn MainView into a placeholder
    
    * Turn MainView into a placeholder (Hilla)
    
    * Clean up pom file and assembly files
    
    * Remove clock
    
    * Remove clock from view
    
    * Improve Docker file
    # Conflicts:
    #	walking-skeleton/pom.xml
    
    * Fix pom (#151)
    
    * Fix order of items in pom file
    
    * Adjust assembly files
    # Conflicts:
    #	walking-skeleton/pom.xml

--- walking-skeleton/src/main/java/com/example/application/taskmanagement/package-info.java
+++ walking-skeleton/src/main/java/com/example/application/taskmanagement/package-info.java
@@ -11,5 +11,14 @@
  * directly to the application package.
  * </p>
  */
+@NullMarked
+//#if ui.framework == "hilla"
+@NonNullApi // Until https://github.com/vaadin/hilla/issues/2612 has been fixed
+//#endif
 package com.example.application.taskmanagement;
 // TODO Remove this package once you have added real features
+
+import org.jspecify.annotations.NullMarked;
+//#if ui.framework == "hilla"
+import org.springframework.lang.NonNullApi;
+//#endif
