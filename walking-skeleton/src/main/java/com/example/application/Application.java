eb0c95791f71af290824901ad4701b4620b6b5dd
Petter Holmstrom <petter@vaadin.com>
2026-02-06 11:46:47 +0200

    Replace Lumo with Aura

--- walking-skeleton/src/main/java/com/example/application/Application.java
+++ walking-skeleton/src/main/java/com/example/application/Application.java
@@ -1,6 +1,6 @@
 package com.example.application;
 
-import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.theme.aura.Aura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +8,7 @@ import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-@StyleSheet(Lumo.STYLESHEET) // Use Aura.STYLESHEET to use Aura instead
-@StyleSheet(Lumo.UTILITY_STYLESHEET)
+@StyleSheet(Aura.STYLESHEET)
 @StyleSheet("styles.css") // Your custom styles
 public class Application implements AppShellConfigurator {
 
