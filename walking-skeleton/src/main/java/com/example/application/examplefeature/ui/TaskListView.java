ed7d51f1b4301d8fd70063a163077af2677c74cc
Artur Signell <artur@vaadin.com>
2026-03-12 11:42:10 +0200

    chore: Use sucess notification

--- walking-skeleton/src/main/java/com/example/application/examplefeature/ui/TaskListView.java
+++ walking-skeleton/src/main/java/com/example/application/examplefeature/ui/TaskListView.java
@@ -1,25 +1,27 @@
 package com.example.application.examplefeature.ui;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Optional;
+
 import com.example.application.base.ui.ViewToolbar;
 import com.example.application.examplefeature.Task;
 import com.example.application.examplefeature.TaskService;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.dom.Style;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Optional;
 
 import static com.vaadin.flow.spring.data.VaadinSpringDataHelpers.toSpringPageRequest;
 
@@ -86,7 +88,8 @@ class TaskListView extends VerticalLayout {
         taskGrid.getDataProvider().refreshAll();
         description.clear();
         dueDate.clear();
-        Notification.show("Task added", 3000, Notification.Position.BOTTOM_END);
+        Notification.show("Task added", 3000, Notification.Position.BOTTOM_END)
+                .addThemeVariants(NotificationVariant.SUCCESS);
     }
 
 }
